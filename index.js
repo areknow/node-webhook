d7a3b611d01ac3273317463cd62fc1fd3d2746e8
Arnaud Crowther <hi@arnaud.cr>
2017-04-27 14:28:11 -0500

    added a complex card post

--- index.js
+++ index.js
@@ -17,12 +17,32 @@ webHooks.add('webhook-more', 'https://outlook.office.com/webhook/9a13f911-e1f1-4
     console.log('webhook-more success')
 }).catch(function(err){console.log(err)})
  
+webHooks.add('webhook-complex', 'https://outlook.office.com/webhook/9a13f911-e1f1-4f6a-868b-def0c81f3a4e@70ebe3a3-5b30-435d-9d67-7716d74ca190/IncomingWebhook/e5cefb5c639943aab4c7ba8046459c4e/d609a152-1ff6-41f5-a3c7-e5a1db19636e').then(function(){
+    console.log('webhook-complex success')
+}).catch(function(err){console.log(err)})
 
 
 
+webHooks.trigger('webhook-simple', {
+    "text": "Hello MS Teams!"
+})
 
-webHooks.trigger('webhook-simple', {"text": "Hello MS Teams!"})
-webHooks.trigger('webhook-more', 
-{"title": "Learn about Office 365 Connectors", 
-"text": "Visit the [Outlook Dev Portal](https://dev.outlook.com) to learn more about Office 365 Connectors!", 
-"themeColor": "EA4300"})
+webHooks.trigger('webhook-more', {
+    "title": "Learn about Office 365 Connectors", 
+    "text": "Visit the [Outlook Dev Portal](https://dev.outlook.com) to learn more about Office 365 Connectors!", 
+    "themeColor": "EA4300"
+})
+
+webHooks.trigger('webhook-complex', {
+    "title": "Learn about Office 365 Connectors", 
+    "text": "Visit the [Outlook Dev Portal](https://dev.outlook.com) to learn more about Office 365 Connectors!", 
+    "themeColor": "EA4300", 
+    "potentialAction": [{
+        "@context": "https://schema.org", 
+        "@type": "ViewAction", 
+        "name": "Open Outlook Dev Center", 
+        "target": ["https://dev.outlook.com"]
+    }]
+})
+
+
